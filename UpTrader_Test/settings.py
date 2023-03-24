7198f7986f06adf2095650b9ca25df60cd330fe0
nktph <nkt.ph.coop@gmail.com>
2023-03-24 19:18:27 +0300

    1) Added a separate menu item drawing function. 2) Removed secret key from settings.py

--- UpTrader_Test/settings.py
+++ UpTrader_Test/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-67)3jv!59l71ia(g0pyp&f_x40)ss2q*o645y)=6=tq*=shn*g'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- menu/templatetags/menu_tags.py
+++ menu/templatetags/menu_tags.py
@@ -6,27 +6,32 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+
+def draw_menu_item(menu_item):
+    html = ''
+    html += '<li class="has-dropdown">'
+    html += f'<a href="{reverse("xxx", args=[menu_item.url])}">{menu_item.name}</a>'
+    return html
+
+
 @register.simple_tag(takes_context=True)
 def draw_menu(context, menu_name):
     menu = MenuItem.objects.all()
-    menu_item = menu.filter(url=menu_name)
-    parents = menu_item[0].get_ancestors()
+    menu_item = menu.filter(url=menu_name).first()
+    parents = menu_item.get_ancestors()
     children = list(menu.filter(parent__name=menu_name))
 
     html = ''
     for parent in parents:
         html += '<ul>'
-        html += '<li class="has-dropdown">'
-        html += f'<a href="{reverse("xxx", args=[parent.url])}">{parent.name}</a>'
+        html += draw_menu_item(parent)
 
     html += '<ul>'
-    html += '<li class="has-dropdown">'
-    html += f'<a href="{reverse("xxx", args=[menu_item[0].url])}"><i>{menu_item[0].name}</i></a>'
+    html += draw_menu_item(menu_item)
 
     for child in children:
         html += '<ul>'
-        html += '<li class="">'
-        html += f'<a href="{reverse("xxx", args=[child.url])}">{child.name}</a>'
+        html += draw_menu_item(child)
         html += '</ul>'
 
     html += '</ul>'
